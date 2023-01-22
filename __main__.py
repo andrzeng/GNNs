0f6dae3c1fe513670769390d6ab8149eddeab6c7
Andrew Zeng <andrew.zeng@protonmail.com>
2023-01-22 11:24:22 -0500

    basic transformer class

--- __main__.py
+++ __main__.py
@@ -1,1 +1,23 @@
-s
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.nn.utils.rnn import pad_sequence
+from torch.autograd import Variable
+import torch.optim as optim
+import numpy as np
+import random
+import math
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+from torch_geometric.data import Data
+
+if __name__ == '__main__':
+    edge_index = torch.Tensor([[0, 1, 1, 2],
+                               [1, 0, 2, 1]])
+    x = torch.Tensor([[-1], [0], [1]])                
+    data = Data(x=x, edge_index=edge_index)
+    data.validate(raise_on_error=True)
+    
